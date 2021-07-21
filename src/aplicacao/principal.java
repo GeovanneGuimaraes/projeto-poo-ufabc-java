838b21056c1c749ed7fe5452be8c609836ece06b
GeovanneGuimaraes <geovanne_guimaraes@hotmail.com>
2021-07-20 21:10:14 -0300

    Criacao da primeira classe - Classe Posicao

--- src/aplicacao/principal.java
+++ src/aplicacao/principal.java
@@ -1,10 +1,14 @@
 package aplicacao;
 
+import tabuleiro.Posicao;
+
 public class principal {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Olá mundo!");
+		//Testando a classe Posicao
+		Posicao posicao = new Posicao(2,  6);
+		System.out.println(posicao);
 
 	}
 
---
+++ src/tabuleiro/Posicao.java
@@ -0,0 +1,36 @@
+package tabuleiro;
+
+public class Posicao {
+	
+	private int coluna;
+	private int linha;
+	
+	public Posicao(int coluna, int linha) {
+		this.coluna = coluna;
+		this.linha = linha;
+	}
+	
+	// Get e Set do atributo coluna
+	public int getColuna() {
+		return coluna;
+	}
+	
+	public void setColuna(int coluna) {
+		this.coluna = coluna;
+	}
+	
+	
+	//Get e Set do atributo linha
+	public int getLinha() {
+		return linha;
+	}
+	
+	public void setLinha(int linha) {
+		this.linha = linha;
+	}
+	
+	// Mostrar a linha e coluna
+	public String toString() {
+		return "Linha: " + linha + " - Coluna: " + coluna;
+ 	}
+}
