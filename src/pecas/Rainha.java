0a47279b19e51ce408aac56956dbc9006bd8dd3a
GeovanneGuimaraes <geovanne_guimaraes@hotmail.com>
2021-08-05 21:16:57 -0300

    Classe Rainha

---
+++ src/pecas/Rainha.java
@@ -0,0 +1,19 @@
+package pecas;
+
+import jogo_de_xadrez.CoresPecas;
+import jogo_de_xadrez.PecaXadrez;
+import tabuleiro.Tabuleiro;
+
+public class Rainha extends PecaXadrez{
+
+	//Construtor da subclasse rainha
+	public Rainha(Tabuleiro tabuleiro, CoresPecas cor) {
+		super(tabuleiro, cor);
+	}
+	
+	//Retorna "Q" (Queen) como Rainha
+	public String toString() {
+		return "♕";
+	}
+	
+}
