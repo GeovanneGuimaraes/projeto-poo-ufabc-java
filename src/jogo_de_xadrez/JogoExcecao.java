0177e6956bbb315b142f1a3ab9a99f004372a838
GeovanneGuimaraes <geovanne_guimaraes@hotmail.com>
2021-08-06 17:52:28 -0300

    Exceção para classe Jogo

---
+++ src/jogo_de_xadrez/JogoExcecao.java
@@ -0,0 +1,12 @@
+package jogo_de_xadrez;
+
+public class JogoExcecao extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public JogoExcecao(String message) {
+		super(message);
+	}
+	
+	
+}
