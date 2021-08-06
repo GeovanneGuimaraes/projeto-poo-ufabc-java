8b3eab7ecd9c1454d318ab1e4cb3daad7d20ae42
GeovanneGuimaraes <geovanne_guimaraes@hotmail.com>
2021-08-05 21:16:42 -0300

    Classe Peao

---
+++ src/pecas/Peao.java
@@ -0,0 +1,18 @@
+package pecas;
+
+import jogo_de_xadrez.CoresPecas;
+import jogo_de_xadrez.PecaXadrez;
+import tabuleiro.Tabuleiro;
+
+public class Peao extends PecaXadrez {
+	
+	//Construtor da subclasse Peao
+	public Peao(Tabuleiro tabuleiro, CoresPecas cor) {
+		super(tabuleiro, cor);
+	}
+	
+	//Retorna a peca Peao "P" 
+	public String toString() {
+		return "♟";
+	}
+}
