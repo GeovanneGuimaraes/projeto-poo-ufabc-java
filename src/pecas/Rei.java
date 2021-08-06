3591fa227731c0169b6c3a41994d02d3ef6b0045
GeovanneGuimaraes <geovanne_guimaraes@hotmail.com>
2021-08-05 21:17:08 -0300

    Classe Rei

---
+++ src/pecas/Rei.java
@@ -0,0 +1,19 @@
+package pecas;
+
+import jogo_de_xadrez.CoresPecas;
+import jogo_de_xadrez.PecaXadrez;
+import tabuleiro.Tabuleiro;
+
+public class Rei extends PecaXadrez{
+
+	//Construtor de Rei
+	public Rei(Tabuleiro tabuleiro, CoresPecas cor) {
+		super(tabuleiro, cor);
+	}
+	
+	//Retorna "R" como rei no tabuleiro
+	public String toString() {
+		return "♚";
+	}
+	
+}
