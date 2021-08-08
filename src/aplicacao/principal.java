765890662a382384f14824bd4390079dcca792d8
GeovanneGuimaraes <geovanne_guimaraes@hotmail.com>
2021-08-08 19:01:39 -0300

    Chamada do método clearScreen e inserido JogoExcecao no loop

--- src/aplicacao/principal.java
+++ src/aplicacao/principal.java
@@ -1,19 +1,38 @@
 package aplicacao;
 
+import java.util.Scanner;
+
 import jogo_de_xadrez.Jogo;
-import tabuleiro.Posicao;
-import tabuleiro.Tabuleiro;
+import jogo_de_xadrez.JogoExcecao;
+import jogo_de_xadrez.JogoPosicao;
+import jogo_de_xadrez.PecaXadrez;
 
 public class Principal {
 
 	public static void main(String[] args) {
-		
-		//Testando o tabuleiro
+
+		Scanner teclado = new Scanner(System.in);
 		Jogo xadrez = new Jogo();
-		interfaceJogo.mostraTabuleiro(xadrez.getPecas());
+
+		// Testando o tabuleiro
+		while (true) {
+			try {
+				interfaceJogo.clearScreen();
+				interfaceJogo.mostraTabuleiro(xadrez.getPecas());
+				System.out.println("");
+				System.out.print("Origem: ");
+				JogoPosicao origem = interfaceJogo.lerJogoPosicao(teclado);
+				System.out.println();
+				System.out.print("Destino: ");
+				JogoPosicao destino = interfaceJogo.lerJogoPosicao(teclado);
+				PecaXadrez pecaCapturada = xadrez.moverPeca(origem, destino);
+			} catch (JogoExcecao e) {
+				System.out.println(e.getMessage());
+				teclado.nextLine();
+			}
+
+		}
 
 	}
-	
-	
 
 }
